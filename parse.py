d243e80fd174d7a3113c801eff3cfc58e66042d5
Daniil <rand1kch@gmail.com>
2020-06-20 02:31:29 +0400

    Add parse lat and long ifexist

--- parse.py
+++ parse.py
@@ -5,7 +5,6 @@ access_token = "28f1ebfa28f1ebfa28f1ebfad528835af6228f128f1ebfa761ce2e835425078a
 session = vk.Session(access_token=access_token)
 vkapi = vk.API(session)
 
-
 # Научиться получать все посты из группы
 # каждый пост форматировать в JSON {Location, posrt, url}
 # Написать функцию по парсингу постов из релевантных групп
@@ -29,10 +28,31 @@ def get_posts(owner_id, vkapi, count):
 
     for item in posts_list:
         post = item['text']
-
+        print(post + "\n")
         post_texts.append([post])
+        try:
+            attachments = item['attachments']
+            if attachments:
+                for attach in attachments:
+                    type = attach['type']
+                    if type == "photo":
+                        photo = attach['photo']
+                        if photo['lat']:
+                            lat = photo['lat']
+                            long = photo['long']
+                            print("Гео: {}".format(lat))
+                            break
+
+        except:
+            print("Error")
 
     return post_texts
 
 
-print(get_posts(owner_id='-109125816', vkapi=vkapi, count=10))
+def search_posts(owner_id, vkapi, count, search_text):
+    posts = []
+
+    posts_list = vkapi.wall.search(owner_id=owner_id, count=50)
+
+
+print(get_posts(owner_id='-109125816', vkapi=vkapi, count=50))
